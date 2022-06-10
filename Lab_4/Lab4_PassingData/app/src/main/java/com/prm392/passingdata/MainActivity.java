b5ef00a8ba2a5636c9f3a9965f4682a851b3866e
duyndse150396 <duyndse@gmail.com>
2022-06-10 07:38:02 +0700

    ADD Lab4_PassingData

---
+++ Lab_4/Lab4_PassingData/app/src/main/java/com/prm392/passingdata/MainActivity.java
@@ -0,0 +1,31 @@
+package com.prm392.passingdata;
+
+import android.content.Intent;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.button);
+
+        button.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+            String [] cityArr = {"Hanoi", "HCM", "Da Nang", "Hai Phong"};
+            Student student = new Student("Nguyen Dinh Duy", "SE150396");
+            Bundle bundle = new Bundle();
+            bundle.putString("string", "Truyen data voi Bundle");
+            bundle.putInt("number", 255);
+            bundle.putStringArray("array", cityArr);
+            bundle.putSerializable("student", student);
+
+            intent.putExtra("Bundle", bundle);
+            startActivity(intent);
+        });
+    }
+}
---
+++ Lab_4/Lab4_PassingData/app/src/main/java/com/prm392/passingdata/SecondActivity.java
@@ -0,0 +1,35 @@
+package com.prm392.passingdata;
+
+import android.content.Intent;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.util.Arrays;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        TextView tvDisplay = findViewById(R.id.tvDisplay);
+        Intent intent = getIntent();
+        Bundle bundle = intent.getBundleExtra("Bundle");
+        if (bundle != null) {
+            String str = bundle.getString("string");
+            int number = bundle.getInt("number");
+            String[] cityArr = bundle.getStringArray("array");
+            StringBuilder arrString = new StringBuilder();
+            for (int i = 0; i < cityArr.length; i++) {
+                if (i == cityArr.length - 1)
+                    arrString.append(cityArr[i]);
+                else arrString.append(cityArr[i]).append(", ");
+            }
+            Student student = (Student) bundle.getSerializable("student");
+            String result = "String: " + str + "\n" + "Number: " + number + "\n" + "Array: " + arrString + "\n" + "Student: " + student.getName() + " - " + student.getId();
+
+            tvDisplay.setText(result);
+        }
+    }
+}
